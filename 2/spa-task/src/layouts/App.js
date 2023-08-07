acbecd4343ed25cb4d73b6d782e355d8fc505049
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-08-07 14:40:20 +0200

    adding components

--- 2/spa-task/src/layouts/App.js
+++ 2/spa-task/src/layouts/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import '../styles/App.css'
 import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './Header';
+import Nav from './Nav';
+import Page from './Page';
+import Footer from './Footer';
 
 function App() {
   return (
     <Router>
-    <div className="App">
+    <div className="app">
      <header>
       { <Header /> }
       </header>
       <main> 
         <aside>
-          {<Navigation />}
+          {<Nav />}
         </aside>
         <section className='page'>
           {<Page />}
