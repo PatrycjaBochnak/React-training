865a526a3c8d9b57cc1ef86113058822a191f685
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-08-07 15:03:35 +0200

    nav

--- 2/spa-task/src/layouts/Nav.js
+++ 2/spa-task/src/layouts/Nav.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/Nav.css";
+
+const list = [
+  { name: "Start", path: "/" },
+  { name: "Produkty", path: "/products" },
+  { name: "Kontakt", path: "/contact" },
+  { name: "Panel admina", path: "/admin" },
+];
 
 const Nav = () => {
-    return (
-    <div>Nav</div>
-    )
-}
+  const menu = list.map((item) => (
+    <li key={item.name}>
+      <NavLink to={item.path}>{item.name}</NavLink>
+    </li>
+  ));
+  return (
+    <nav className="main">
+      <ul>{menu}</ul>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
