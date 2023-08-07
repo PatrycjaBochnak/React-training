81f0e1c6960a18fabc753c4a577afad7a9224670
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-08-07 14:06:13 +0200

    initial commit

---
+++ 2/spa-task/src/layouts/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../styles/App.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+     <header>
+      { <Header /> }
+      </header>
+      <main> 
+        <aside>
+          {<Navigation />}
+        </aside>
+        <section className='page'>
+          {<Page />}
+        </section>
+      </main>
+      <footer>
+        {<Footer/>}
+      </footer>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
