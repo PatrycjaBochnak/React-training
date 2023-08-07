ebe0597e514ad3c27f895f825498253edaa31af6
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-08-07 17:12:38 +0200

    styling

--- 2/spa-task/src/pages/HomePage.js
+++ 2/spa-task/src/pages/HomePage.js
@@ -1,8 +1,39 @@
 import React from 'react';
-
+import Article from '../components/Article';
+const articles = [
+ {
+    id: 1,
+    title: "Czym jest teoria strun",
+    author: "Jan Nowak",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+ },
+ {
+    id: 2,
+    title: "Wielki wybuch",
+    author: "Jonasz Kadzidło",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+ },
+ {
+    id: 3,
+    title: "Klawiatura wifi",
+    author: "Katarzyna Skura",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+ },
+ {
+    id: 4,
+    title: "Jakdojade",
+    author: "Robert M",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+ }
+]
 const HomePage = () => {
+    const artList = articles.map(article => (
+        <Article key={article.id} {...article}/>
+    ))
     return (
-        <div>Strona startowa</div>
+        <div className='home'>
+            {artList}
+            </div>
     )
 }
 
